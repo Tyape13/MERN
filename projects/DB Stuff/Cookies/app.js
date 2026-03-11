1bb94aa1d256e3a61f03f02a340e17b861ffcb7a
Tyape13 <sameerbohara13@gmail.com>
2026-03-11 21:21:33 +0545

    up to date

--- projects/DB Stuff/Cookies/app.js
+++ projects/DB Stuff/Cookies/app.js
@@ -6,7 +6,7 @@ const session = require('express-session');
 const app = express();
 
 
-const sessionOptions = {secret: 'mysecret', resave: false, saveUninitialized: false};
+const sessionOptions = {secret: 'mysecret', resave: false, saveUninitialized: false}; // resave false means that the session will not be saved back to the session store if it was not modified during the request. saveUninitialized false means that a session will not be created until something is stored in it. This can help reduce server storage usage and improve performance by avoiding unnecessary sessions.
 
 app.use(session(sessionOptions)); // just this one line is enough to use session in every page cause it's for "/"
 
@@ -17,14 +17,25 @@ app.get("/", (req, res) => {
     res.send("Cookie has been set!");
 });
 
-app.get("/sessioncount" , (req, res) => {
-    if(req.session.count){
-        req.session.count++;
-    } else {
-        req.session.count = 1;
-    }
-    res.send(`You have visited this page ${req.session.count} times.`);
+
+app.get("/register", (req, res) =>{
+    let {name= "Anonymous"} = req.params; // test domain.com/register?name=JohnDoe.
+    console.log(req.session);
+    req.session.name = name; // now this is going to be global variable which can be accessed by any tab. NICE.
+    res.redirect("/hello"); 
 });
+
+app.get("/hello", (req, res) => {
+    res.send(req.session.name);
+});
+// app.get("/sessioncount" , (req, res) => {
+//     if(req.session.count){
+//         req.session.count++;
+//     } else {
+//         req.session.count = 1;
+//     }
+//     res.send(`You have visited this page ${req.session.count} times.`);
+// });
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
